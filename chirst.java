c80e43c374ffd994437037b83350a6d282a404bb
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 16:10:53 +0530

    Add files via upload

---
+++ chirst.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class vote
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a==25)
+    {
+        System.out.println("CIRSTMAS");
+    }
+    else{
+        System.out.println("ORDINARY");
+    }
+}
+}   
