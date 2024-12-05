6701bc3f41d9d9ba24f46d0e546753db55495738
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 16:05:51 +0530

    Add files via upload

---
+++ tala.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class vote
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a==7)
+    {
+        System.out.println("THALA");
+    }
+    else{
+        System.out.println("SADGE");
+    }
+}
+}   
