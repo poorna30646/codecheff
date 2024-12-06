8e4038a6dadcc52dd8281a2ce565a680a782cbbd
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 11:52:27 +0530

    Add files via upload

---
+++ foodcosts.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int d=a*6+b;
+        System.out.println(d);
+        
+       
+    }
+}
