3163c353e09c111697d6e99ed0f666b642ce036b
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 15:01:53 +0530

    Add files via upload

---
+++ ioi2024.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<=8)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+       }
+    }
