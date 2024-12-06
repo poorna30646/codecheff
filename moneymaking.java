dc2b39fbef27e3208b056b41779f82996d76c308
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 13:51:00 +0530

    Add files via upload

---
+++ moneymaking.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a*5000;
+        int d=b*9800;
+        int e=c+d;
+        System.out.println(e);
+        
+        
+    }
+}
