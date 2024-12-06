8279f845f977805e06495c630a5892ea6dbcdefa
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 13:36:56 +0530

    Add files via upload

---
+++ chefandparole.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        if(t>=7)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
