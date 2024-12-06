6cb39f5df0e32c968b12a7d35f36897daf03a131
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 11:21:04 +0530

    Add files via upload

---
+++ codecheffround.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a==4)
+        {
+            System.out.println("YES");
+
+        }
+        else{
+            System.out.println("NO");
+        }
+       
+    }
+}
