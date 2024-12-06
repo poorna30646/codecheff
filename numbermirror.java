7caae766d5d9155a130e7a8f5b05e7ad4c50d83c
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 13:30:29 +0530

    Add files via upload

---
+++ numbermirror.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        System.out.println(t);
+    }
+}
