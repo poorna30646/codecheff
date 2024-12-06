1cefe665e7abfcfb613b0f64dd250a5d8a95144f
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 14:45:48 +0530

    Add files via upload

---
+++ advitiya.java
@@ -0,0 +1,23 @@
+ import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a==16)
+        {
+            System.out.println("ADVITIYA");
+        }
+        else if(a==17)
+        {
+            System.out.println("ADVITIYA");
+        }
+        else if(a==18)
+        {
+            System.out.println("ADVITIYA");
+        }
+       else{
+        System.out.println("WAITING FOR ADVITIYA");
+       }
+    }
+}
