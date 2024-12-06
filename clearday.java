848b2a9dd1fea793f7b35f16642fe1688a96de92
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 11:15:27 +0530

    Add files via upload

---
+++ clearday.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int d=7;
+        int c=d-a-b;
+        System.out.println(c);
+    }
+}
