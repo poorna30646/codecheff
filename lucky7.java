cee79694d3b40b5ba6d1046ec513b47736d4aca3
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 11:07:35 +0530

    Add files via upload

---
+++ lucky7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        char[] charArray=a.toCharArray();
+        System.out.println(charArray[6]);
+    }
+}
