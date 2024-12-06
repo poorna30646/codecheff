eed39136c3224e8d96d26d9716500a80e4f1455f
poorna30646 <thotipoornachandra@gmail.com>
2024-12-06 13:42:01 +0530

    Add files via upload

---
+++ doublerent.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int a=t*2;
+        System.out.println(a);
+        
+    }
+}
