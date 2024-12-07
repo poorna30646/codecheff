5fb09c8686f5d86f5f4db86b9b43aad0241141d9
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 19:34:11 +0530

    Add files via upload

---
+++ waterconsumption.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+        int a=sc.nextInt();
+        if(a>=2000)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+    
+}
+	
+}
+
