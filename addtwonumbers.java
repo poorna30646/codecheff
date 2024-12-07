07cab7c090ca3341f8a2555588ea55f25c2682f0
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 09:37:08 +0530

    Add files via upload

---
+++ addtwonumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class freefire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+      {
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=b+c;
+            System.out.println(d);
+        }
+    }
+}
