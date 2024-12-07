66e91912823a49a8904cc7d6ba4706a8f019273c
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 20:41:23 +0530

    Add files via upload

---
+++ yogaday.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=a/12;
+		if(b<=12)
+		{
+		    System.out.println(b);
+		}
+		else{
+		    System.out.println("0");
+		}
+
+	}
+}
