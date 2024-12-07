a46b37dd86b7db3951c7c1553444f99f71eeee06
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 20:54:03 +0530

    Add files via upload

---
+++ 404notfound.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==404)
+		{
+		    System.out.println("NOT FOUND");
+		}
+		else{
+		    System.out.println("FOUND");
+		}
+
+	}
+}
+
