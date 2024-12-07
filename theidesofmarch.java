db66d85d61a4f7b8f788185d00b4da7c5dc2bba5
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 11:47:29 +0530

    Add files via upload

---
+++ theidesofmarch.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class free
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a==15)
+        System.out.println("YES");
+        else System.out.println("NO");
+    }
+} 
