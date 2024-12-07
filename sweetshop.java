0595f241c9f1bfc389bb94d02508a338722fac63
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 20:15:35 +0530

    Add files via upload

---
+++ sweetshop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=b*10;
+		int d=a-c;
+		int e=d/20;
+		System.out.println(e);
+
+	}
+}
