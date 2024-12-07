54e85e3a67630ff8c6b59719e71375ca67093d40
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 19:42:23 +0530

    Add files via upload

---
+++ mastercheffinals.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int a=sc.nextInt();
+		    if(a<=10)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
+
