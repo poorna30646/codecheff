a649e0cc8eb6b177f7fe7ba638f47048b6be075e
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 09:41:14 +0530

    Add files via upload

---
+++ acplese.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class freefire{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        if(t>30)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
