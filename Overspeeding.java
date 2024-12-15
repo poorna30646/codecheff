c599cc08a710934953b05fc2c6518def92308276
poorna30646 <thotipoornachandra@gmail.com>
2024-12-15 20:12:47 +0530

    Overspeeding.java

---
+++ Overspeeding.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    if(a<=40)
+	    {
+	        System.out.println("NO");
+	    }
+	    else{
+	        System.out.println("YES");
+	    }
+
+	}
+}
