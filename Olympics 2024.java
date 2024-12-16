c0c87e3296095736d83878420e47fdc7fc864414
poorna30646 <thotipoornachandra@gmail.com>
2024-12-16 11:02:02 +0530

    Olympics 2024.java

---
+++ Olympics 2024.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=5-a;
+        int e=5-b;
+        int f=5-c;
+        System.out.println(d+e+f);
+
+	}
+}
