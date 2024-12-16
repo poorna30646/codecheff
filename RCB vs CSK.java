58168b3916f535b378f48ee9690835cf0a69fb23
poorna30646 <thotipoornachandra@gmail.com>
2024-12-16 10:33:22 +0530

    RCB vs CSK.java

---
+++ RCB vs CSK.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a-b;
+		int d=b-a;
+		if(c>=18)
+		{
+		    System.out.println("RCB");
+		}
+		else{
+		    System.out.println("CSK");
+		}
+		
+
+	}
+}
