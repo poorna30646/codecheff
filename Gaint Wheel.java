8ab82439e5b2ae6c427e9f45a06bb40ad6d03030
poorna30646 <thotipoornachandra@gmail.com>
2024-12-31 22:14:29 +0530

    Gaint Wheel.java

---
+++ Gaint Wheel.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.util.Scanner;
+class fire
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=60)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
