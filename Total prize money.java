148e248266853f557f7688277970d2477e0cbe67
poorna30646 <thotipoornachandra@gmail.com>
2025-01-03 19:31:39 +0530

    Total prize money.java

---
+++ Total prize money.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class fire
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=a*10+b*90;
+            System.out.println(c);
+        }
+        
+    }
+}
