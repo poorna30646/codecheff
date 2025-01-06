d56af0013f69857e520c2224eeb601aa47e9ca1f
poorna30646 <thotipoornachandra@gmail.com>
2025-01-06 15:57:04 +0530

    No of credits.java

---
+++ No of credits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class fire
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            System.out.println(a*4+b*2+0);
+        }
+    }
+}
